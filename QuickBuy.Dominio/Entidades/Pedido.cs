2e60edaf07855c6b1051aa0c4e15cc026a1c706d
ThomasDevRossi <thomas__rossi@outlook.com>
2021-04-07 22:14:30 -0300

    20. Implementando Validação nas Classes ( Nada de Exceptions )

--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -1,18 +1,38 @@
-﻿using System;
+﻿using QuickBuy.Dominio.ObjetoDeValor;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace QuickBuy.Dominio.Entidades
 {
-    public class Pedido
+    public class Pedido : Entidade
     {
         public int Id { get; set; }
         public DateTime DataPedido { get; set; }
         public int UsuarioId { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public int NumeroEndereco { get; set; }
+        public int FormaPagamentoId { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
 
         /// <summary>
-        /// Pedido deve ter pelo menos um pedido
-        /// ou muitos pedidos
+        /// Pedido deve ter pelo menos um item de pedido
+        /// ou muitos itens de pedido
         /// </summary>
         public ICollection<ItemPedido> ItensPedido { get; set; }
+
+        public override void Validade()
+        {
+            LimparMensagensValidacao();
+
+            if (!ItensPedido.Any())
+                AdicionarMensagem("Preenchimento Obrigatorio - Item de Pedido não pode ficar vazio");
+            if (string.IsNullOrEmpty(CEP))
+                AdicionarMensagem("Preenchimento Obrigatorio - CEP");
+        }
     }
 }
